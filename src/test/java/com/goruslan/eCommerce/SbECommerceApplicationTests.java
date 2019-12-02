fd01e2d48ca97a25a02df0159fac3ba13e2e4024
Ruslan Temirkhanov <rusl0424@gmail.com>
2019-12-02 16:32:54 -0500

    init commit

---
+++ src/test/java/com/goruslan/eCommerce/SbECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goruslan.eCommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
