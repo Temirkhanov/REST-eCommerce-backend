b57b0baef8433707732e486de2b9ab1e1d346a70
Ruslan Temirkhanov <rusl0424@gmail.com>
2020-01-02 17:39:58 -0500

    Added User model

---
+++ src/main/java/com/goruslan/eCommerce/entity/User.java
@@ -0,0 +1,23 @@
+package com.goruslan.eCommerce.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="username")
+    private String username;
+
+    @Column(name="password")
+    private String password;
+
+
+}
