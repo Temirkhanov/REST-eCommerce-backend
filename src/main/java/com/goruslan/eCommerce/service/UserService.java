2f9e180a812c987ba26631c10789d1b0ee42c62f
Ruslan Temirkhanov <rusl0424@gmail.com>
2020-01-03 20:29:01 -0500

    Added Service Interface and Impl for User.

---
+++ src/main/java/com/goruslan/eCommerce/service/UserService.java
@@ -0,0 +1,22 @@
+package com.goruslan.eCommerce.service;
+
+import com.goruslan.eCommerce.entity.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+public interface UserService {
+    User saveUser(User user);
+
+    // Method 'save' can be used for updating and inserting operation
+    User upadteUser(User user);
+
+    void deleteUser(Long userId);
+
+    User findByUsername(String username);
+
+    List<User> findAllUsers();
+
+    Long numberOfUsers();
+}
---
+++ src/main/java/com/goruslan/eCommerce/service/UserServiceImpl.java
@@ -0,0 +1,52 @@
+package com.goruslan.eCommerce.service;
+
+import com.goruslan.eCommerce.entity.User;
+import com.goruslan.eCommerce.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // Will be added it later to web security config.
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public User saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    // Method 'save' can be used for updating and inserting operation
+    @Override
+    public User upadteUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void deleteUser(Long userId) {
+        userRepository.deleteById(userId);
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username).orElse(null);
+    }
+
+    @Override
+    public List<User> findAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Long numberOfUsers() {
+        return userRepository.count();
+    }
+}
